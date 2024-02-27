df61b0db45d493206dc5a63cd0feeb451775cbc2
Enor Zilkiqi <ez222dc@student.lnu.se>
2024-02-27 15:37:18 +0100

    testing dynamic theme settings

--- src/js/DetectMode.js
+++ src/js/DetectMode.js
@@ -23,12 +23,14 @@ function DetectMode(container) {
     this.lightMode = function () {
         this.container.style.backgroundColor = '#278664';
         this.container.setAttribute('data-mode', 'light');
+        document.querySelector('meta[name="theme-color"]').setAttribute('content', '#278664');
         this.updateIcons();
     }
     
     this.darkMode = function () {
         this.container.style.backgroundColor = '#4e594a';
         this.container.setAttribute('data-mode', 'dark');
+        document.querySelector('meta[name="theme-color"]').setAttribute('content', '#4e594a');
         this.updateIcons();
     }
     
@@ -41,52 +43,3 @@ function DetectMode(container) {
         this.updateIcons();
     }
 }
-
-
-
-/*
-function DetectMode(container) {
-    this.container = container;
-
-    this.detectIcons = function (settingsIconElm, closeModeIconElm, closeSettingsIconElm) {
-        this.settingsIconElm = settingsIconElm;
-        this.closeModeIconElm = closeModeIconElm;
-        this.closeSettingsIconElm = closeSettingsIconElm;
-        this.updateIcons();
-    }
-
-    this.updateIcons = function () {
-        console.log('updateIcons called with', this.settingsIconElm, this.closeModeIconElm, this.closeSettingsIconElm);
-        var mode = this.container.getAttribute('data-mode');
-        if (mode === 'light') {
-            this.settingsIconElm.src = '../src/img/settings_icon_1.png';
-            this.closeModeIconElm.src = '../src/img/close_icon_1.png';
-            this.closeSettingsIconElm.src = '../src/img/close_icon_1.png';
-        } else if (mode === 'dark') {
-            this.settingsIconElm.src = '../src/img/settings_icon_2.png';
-            this.closeModeIconElm.src = '../src/img/close_icon_2.png';
-            this.closeSettingsIconElm.src = '../src/img/close_icon_2.png';
-        }
-    }
-
-    this.lightMode = function () {
-        this.container.style.backgroundColor = 'rgb(39, 134, 100)';
-        this.container.setAttribute('data-mode', 'light');
-        this.updateIcons();
-    }
-    
-    this.darkMode = function () {
-        this.container.style.backgroundColor = 'rgb(78, 89, 74)';
-        this.container.setAttribute('data-mode', 'dark');
-        this.updateIcons();
-    }
-
-    this.detect = function () {
-        if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-            this.darkMode();
-        } else {
-            this.lightMode();
-        }
-    }
-}
-*/
